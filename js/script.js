17cd8d8534a5f361c2f7340b90b77abf80b76134
sersha01 <skperson01@gmail.com>
2021-12-28 20:35:57 +0530

    contact cleared

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".check").addEventListener("keypress", function (evt) {
+    if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }
+});
