55efc666495eb382b3e5f121f72a86a57a227d23
sersha01 <skperson01@gmail.com>
2021-12-29 22:32:05 +0530

    validation success

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-(function($) {
+(function ($) {
 
 	"use strict";
 
 	/* ----------------------------------------------------------- */
 	/*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
-    /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
 
 	function stop_videos() {
 		var video = document.getElementById("video");
@@ -14,11 +14,11 @@
 		$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
 	}
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 
 		/* ----------------------------------------------------------- */
 		/*  STOP VIDEOS
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		$('.slideshow nav span').on('click', function () {
 			stop_videos();
@@ -26,39 +26,39 @@
 
 		/* ----------------------------------------------------------- */
 		/*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		$(".revealator-delay1").addClass('no-transform');
 
 		/* ----------------------------------------------------------- */
 		/*  PORTFOLIO GALLERY
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		if ($('.grid').length) {
-			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+			new CBPGridGallery(document.getElementById('grid-gallery'));
 		}
 
 		/* ----------------------------------------------------------- */
 		/*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".grid figure").on('click', function() {
+		$(".grid figure").on('click', function () {
 			$("#navbar-collapse-toggle").addClass('hide-header');
 		});
 
 		/* ----------------------------------------------------------- */
 		/*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".nav-close").on('click', function() {
+		$(".nav-close").on('click', function () {
 			$("#navbar-collapse-toggle").removeClass('hide-header');
 		});
-		$(".nav-prev").on('click', function() {
+		$(".nav-prev").on('click', function () {
 			if ($('.slideshow ul li:first-child').hasClass('current')) {
 				$("#navbar-collapse-toggle").removeClass('hide-header');
 			}
 		});
-		$(".nav-next").on('click', function() {
+		$(".nav-next").on('click', function () {
 			if ($('.slideshow ul li:last-child').hasClass('current')) {
 				$("#navbar-collapse-toggle").removeClass('hide-header');
 			}
@@ -66,7 +66,7 @@
 
 		/* ----------------------------------------------------------- */
 		/*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
 		var item = $(".grid li figure");
 		var elementsLength = item.length;
@@ -76,50 +76,52 @@
 
 		/* ----------------------------------------------------------- */
 		/*  AJAX CONTACT FORM
-        /* ----------------------------------------------------------- */
+		/* ----------------------------------------------------------- */
 
-		$(".contactform").on("submit", function() {
+		$("#submit-form").submit((e)=>{
 			$(".output_message").text("Sending...");
+			e.preventDefault()
 
 			var form = $(this);
 			$.ajax({
-				url: form.attr("action"),
-				method: form.attr("method"),
-				data: form.serialize(),
-				success: function(result) {
-					if (result == "success") {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Message Sent!");
-					} else {
-						$(".tabs-container").css("height", "440px");
+				url: "https://script.google.com/macros/s/AKfycbx-S7dBP9B2t_4p8yiB10rsPfskw2vcWLHTowrB4ZC-pC8BOo7LNLpm87VlpuxqCgs/exec",
+				method: "post",
+				data: $("#submit-form").serialize(),
+				success: function (response) {
+					$(".form-inputs").css("display", "none");
+					$(".box p").css("display", "none");
+					$(".contactform").find(".output_message").addClass("success");
+					$(".output_message").text("Message Sent!");
+				},
+				error: function (err) {
+					$(".tabs-container").css("height", "440px");
 
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Error Sending!");
-					}
+					$(".contactform").find(".output_message").addClass("error");
+					$(".output_message").text("Error Sending!");
 				}
-			});
+			}
+			);
 
-			return false;
-		});
 
+		return false;
 	});
 
-	$(document).keyup(function(e) {
+});
 
-		/* ----------------------------------------------------------- */
-		/*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
-        /* ----------------------------------------------------------- */
-		if (e.keyCode === 27) {
-			stop_videos();
-			$('.close-content').click();
-			$("#navbar-collapse-toggle").removeClass('hide-header');
-		}
-		if ((e.keyCode === 37) || (e.keyCode === 39)) {
-			stop_videos();
-		}
-	});
+$(document).keyup(function (e) {
+
+	/* ----------------------------------------------------------- */
+	/*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
+	/* ----------------------------------------------------------- */
+	if (e.keyCode === 27) {
+		stop_videos();
+		$('.close-content').click();
+		$("#navbar-collapse-toggle").removeClass('hide-header');
+	}
+	if ((e.keyCode === 37) || (e.keyCode === 39)) {
+		stop_videos();
+	}
+});
 
 
-})(jQuery);
+}) (jQuery);
