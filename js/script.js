cd159e367b8f4a87fff6fb4a58c144664d1a7116
sersha01 <skperson01@gmail.com>
2021-12-29 12:53:56 +0530

    form almost compleated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-document.querySelector(".check").addEventListener("keypress", function (evt) {
+document.querySelector(".number").addEventListener("keypress", function (evt) {
     if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }else if (document.getElementById("number").value.length == 10) {
+        evt.preventDefault();
+    }
+});
+document.querySelector(".name").addEventListener("keypress", function (evt) {
+    if ( evt.which < 65 || evt.which > 90 && evt.which <97 || evt.which > 122)
     {
         evt.preventDefault();
     }
 });
+
+
+document.querySelector(".message").addEventListener("keypress", function (evt) {
+    if ( evt.which == 32)
+    {
+        if (document.getElementById("message").value == '') {
+            evt.preventDefault();
+        }
+    }
+});
+
+
